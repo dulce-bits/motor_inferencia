15fd3f110bfea2acdec065d8176f35e2b45e63e0
Dulce C <dulceleste.cr@gmail.com>
2019-06-01 19:27:17 -0500

    archivo BC para pruebas

---
+++ generador.py
@@ -0,0 +1,28 @@
+from Regla import Regla
+
+#Reglas y base de conocimiento de ejemplo para una computadora que no sirve
+
+class Generador:
+
+    def GenerarBC(self,filename):   #Aqui se definen las reglas X->Y|-Conclusion
+        contador = 1  # el indice de la regla
+        f = open(filename,'r')
+        reglas=[]
+        for r in f:#Lee renglon por renglon en el archivo f
+            aux = r.split() # lista con cada palabra
+            aux2 = [] # guarda la premisa
+            for p in aux:
+                if p == '->':
+                    break
+                aux2.append(p)            
+            reglas.append(Regla(aux2,[aux[-1]],contador))
+            contador += 1 
+        f.close()
+        return reglas  
+
+    def GenerarBH(self,n): 
+        hechos=[]
+        for i in range(n):
+            hechos.append(input())
+
+        return hechos
